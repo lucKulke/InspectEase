10f537704543889e4769a9a243d174b54df4571c
lucKulke <lucaskulke98@gmail.com>
2025-04-04 15:28:08 +0200

    working on email confirm process

--- next-app-supabase/app/auth/confirm/route.ts
+++ next-app-supabase/app/auth/confirm/route.ts
@@ -1,35 +1,36 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
-
 import { createClient } from "@/utils/supabase/server";
 
-// Creating a handler to a GET request to route /auth/confirm
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = "/";
 
-  // Create redirect link without the secret token
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete("token_hash");
-  redirectTo.searchParams.delete("type");
+  // Default redirect path (after successful confirmation)
+  let nextPath = "/";
 
   if (token_hash && type) {
     const supabase = await createClient();
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash });
 
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
     if (!error) {
-      redirectTo.searchParams.delete("next");
+      // If user was invited, send them to reset their password
+      if (type === "invite") {
+        nextPath = "/reset-password";
+      }
+
+      const redirectTo = request.nextUrl.clone();
+      redirectTo.pathname = nextPath;
+      redirectTo.searchParams.delete("token_hash");
+      redirectTo.searchParams.delete("type");
+
       return NextResponse.redirect(redirectTo);
     }
   }
 
-  // return the user to an error page with some instructions
-  redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo);
+  // Redirect to an error page if token verification fails
+  const errorRedirect = request.nextUrl.clone();
+  errorRedirect.pathname = "/error";
+  return NextResponse.redirect(errorRedirect);
 }
