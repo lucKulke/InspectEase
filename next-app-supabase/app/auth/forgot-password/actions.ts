f9af93384f229fbb7075a945115df5185928e7f0
lucKulke <lucaskulke98@gmail.com>
2025-06-24 15:06:47 +0200

    try fixing password reset

--- next-app-supabase/app/auth/forgot-password/actions.ts
+++ next-app-supabase/app/auth/forgot-password/actions.ts
@@ -4,9 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function forgotPassword(email: string) {
   const supabase = await createClient();
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: "/auth/set-password",
-  });
+  const { error } = await supabase.auth.resetPasswordForEmail(email);
   console.log("forgot password", error);
   return error;
 }
