d6e913f2802a47a9e303aa066014b0c9d0819971
lucKulke <lucaskulke98@gmail.com>
2025-10-14 10:28:05 +0200

    started react native app

---
+++ react-native-app/my-expo-app/app/index.tsx
@@ -0,0 +1,10 @@
+import 'global.css';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Ey Eddy du kek</Text>
+    </View>
+  );
+}
